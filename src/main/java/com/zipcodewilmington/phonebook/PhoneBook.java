aa5e6200e40571007fd0e6977200c4db554507c0
Fenniless <fenniless@gmail.com>
2019-02-16 13:08:45 -0500

    all wrapped

--- src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
+++ src/main/java/com/zipcodewilmington/phonebook/PhoneBook.java
@@ -1,7 +1,65 @@
 package com.zipcodewilmington.phonebook;
-
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+import java.util.TreeMap;
 /**
  * Created by leon on 1/23/18.
  */
+
 public class PhoneBook {
+
+    TreeMap<String, List<String>> phoneBook;
+
+    public PhoneBook() {
+
+        this.phoneBook = new TreeMap<String, List<String>>();
+    }
+
+    public void add(String name, String phoneNumber) {
+
+            if(!phoneBook.containsKey(name)){
+                List<String> numbers = new ArrayList<String>();
+                numbers.add(phoneNumber);
+                phoneBook.put(name, numbers);
+            } else {
+                List<String> numbers = phoneBook.get(name);
+                numbers.add(phoneNumber);
+            }
+        }
+
+
+    public void remove(String name) {
+
+        phoneBook.remove(name);
+    }
+
+    public String lookup(String name) {
+        List<String> result = phoneBook.get(name);
+
+        return result.toString();
+    }
+
+    public String reverseLookup(String phoneNumber) {
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            if (entry.getValue().contains(phoneNumber)) {
+                return entry.getKey();
+            }
+        } return "this entry does not exist";
+    }
+
+    public String display() {
+
+        for (Map.Entry<String, List<String>> entry : phoneBook.entrySet()) {
+            String key = entry.getKey();
+            List<String> value = entry.getValue();
+            String result = (key +" : " +value);
+            return result;
+        }return "no entries";
+    }
+
+    public TreeMap<String, List<String>> getMap(){
+
+        return phoneBook;
+    }
 }
--- src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
+++ src/test/java/com/zipcodewilmington/phonebook/PhoneBookTest.java
@@ -1,7 +1,97 @@
 package com.zipcodewilmington.phonebook;
+import org.junit.Assert;
+import org.junit.Test;
+import org.junit.Before;
+
+import java.util.List;
+import java.util.TreeMap;
+import java.util.Map;
 
 /**
  * Created by leon on 1/23/18.
  */
 public class PhoneBookTest {
+
+
+    @Test
+    public void add() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+
+        String name = "joe";
+        String phoneNumber = "1234567890";
+        //when
+        phoneBook.add(name, phoneNumber);
+        //then
+        Assert.assertTrue(phoneBook.lookup(name).contains(phoneNumber));
+    }
+    @Test
+    public void add2() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+
+        String name = "joe";
+        String phoneNumber = "1234567890";
+        String phoneNumber2 = "0987654321";
+        //when
+        phoneBook.add(name, phoneNumber);
+        phoneBook.add(name, phoneNumber2);
+        //then
+        Assert.assertTrue(phoneBook.lookup(name).contains(phoneNumber));
+    }
+
+    @Test
+    public void remove() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        TreeMap<String, List<String>> map = phoneBook.getMap();
+        String name = "joe";
+        //when
+        phoneBook.add("joe", "1234567890");
+        phoneBook.remove(name);
+        //then
+        Assert.assertEquals(0, map.size());
+    }
+
+    @Test
+    public void lookup() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "joe";
+        String phoneNumber = "1234567890";
+        //when
+        phoneBook.add(name, phoneNumber);
+        String expected = "[" + phoneNumber +"]";
+        String actual = phoneBook.lookup(name);
+        //then
+        Assert.assertEquals(expected, actual);;
+    }
+
+    @Test
+    public void reverseLookup() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "joe";
+        String phoneNumber = "1234567890";
+        //when
+        phoneBook.add(name, phoneNumber);
+        //then
+        String expected = name;
+        String actual = phoneBook.reverseLookup(phoneNumber);
+        Assert.assertEquals(actual, expected);
+    }
+
+    @Test
+    public void display() {
+        //given
+        PhoneBook phoneBook = new PhoneBook();
+        String name = "joe";
+        String phoneNumber = "1234567890";
+        //when
+        phoneBook.add(name, phoneNumber);
+        String expected = "joe : [1234567890]";
+
+        Assert.assertEquals(phoneBook.display(), expected);
+
+    }
 }
